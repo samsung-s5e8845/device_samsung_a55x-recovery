a17ee2457a8f726b65107fb803ffbbf5d628e517
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-06-02 03:21:28 +0200

    essi: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-A556E Script version: 3.0.0

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_essi-user
+add_lunch_combo omni_essi-userdebug
+add_lunch_combo omni_essi-eng
